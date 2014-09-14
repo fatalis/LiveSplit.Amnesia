432850a041ebd8881a4bdbdb94f224d4c4076711
Fatalis <fatalis.twitch@gmail.com>
2014-09-14 11:02:09 -0500

    Initial commit

---
+++ AmnesiaFactory.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using LiveSplit.Amnesia;
+using LiveSplit.UI.Components;
+using System;
+using LiveSplit.Model;
+
+[assembly: ComponentFactory(typeof(AmnesiaFactory))]
+
+namespace LiveSplit.Amnesia
+{
+    public class AmnesiaFactory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { return "Amnesia"; }
+        }
+
+        public string Description
+        {
+            get { return "Load time remover for Amnesia: The Dark Descent"; }
+        }
+
+        public ComponentCategory Category
+        {
+            get { return ComponentCategory.Control; }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new AmnesiaComponent(state);
+        }
+
+        public string UpdateName
+        {
+            get { return this.ComponentName; }
+        }
+
+        public string UpdateURL
+        {
+            get { return "http://fatalis.hive.ai/livesplit/update/"; }
+        }
+
+        public Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        public string XMLURL
+        {
+            get { return this.UpdateURL + "Components/update.LiveSplit.Amnesia.xml"; }
+        }
+    }
+}
